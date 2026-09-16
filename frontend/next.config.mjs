b58eb7cd4c754fcd806680454c09cf615400213d
Plain-Guy <mazlan.ma57@gmail.com>
2026-09-16 23:41:04 +0700

    update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,17 +8,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: '**',
       },
       {
         protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '3001',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3001',
+        hostname: '**',
       },
     ],
   },
